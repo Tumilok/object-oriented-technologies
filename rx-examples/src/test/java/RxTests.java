b44a9cebaaa98a902cbb553147da5405a107a8b9
tumilok <vladislav.tumilovich@gmail.com>
2020-10-26 19:00:55 +0100

    Complete example 11

--- rx-examples/src/test/java/RxTests.java
+++ rx-examples/src/test/java/RxTests.java
@@ -145,10 +145,22 @@ public class RxTests {
      * Example 11: Combining parallel streams.
      */
     @Test
-    public void loadMoviesFromManySourcesParallel() {
+    public void loadMoviesFromManySourcesParallel() throws InterruptedException {
         // Static merge solution
+        var movieReader = new MovieReader();
 
+        Observable<Movie> movies1 = movieReader.getMoviesAsStream(MOVIES1_DB)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(movie -> print(movie, Color.GREEN));
 
+        Observable<Movie> movies2 = movieReader.getMoviesAsStream(MOVIES2_DB)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(movie -> print(movie, Color.BLUE));
+
+        Observable.merge(movies1, movies2)
+                .subscribe(movie -> print("RECEIVED: " + movie, Color.RED));
+
+        Thread.sleep(10000);
 
         // FlatMap solution:
         final MovieDescriptor movie1Descriptor = new MovieDescriptor(MOVIES1_DB, Color.GREEN);
